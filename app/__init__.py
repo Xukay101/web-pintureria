4762cbf06c0086dd4a150c3a08f846ba5d4dd824
Xukay101 <zaca03zaca@gmail.com>
2023-05-26 16:43:54 -0300

    Update hero and added animations

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,8 @@ def create_app():
     def index():
         return render_template('index.html')
 
+    @app.route('/test')
+    def test():
+        return render_template('base.html')
+
     return app
