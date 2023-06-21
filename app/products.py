7b5a81c24af3e29ea9a7d13a420574cc17e6bd53
Joel <f.cabral@itecriocuarto.org.ar>
2023-06-21 20:38:46 -0300

    Finish cart

--- app/products.py
+++ app/products.py
@@ -1,3 +1,4 @@
+from .helpers import get_products
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 import json
 bp = Blueprint('products', __name__)
@@ -5,11 +6,3 @@ bp = Blueprint('products', __name__)
 @bp.route('/products')
 def products():
     return render_template('products.html', products=get_products())
-    
-
-def get_products():
-    with open("app/products.json", "r") as file:
-        content = file.read()
-    products = json.loads(content)
-    return products
-    
